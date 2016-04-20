7559ab7c6929079a271572e2a00a307cecd7d950
Nicola Bortignon <info@nicolabortignon.com>
2016-04-20 16:53:10 -0700

    adding React

---
+++ src/reducers/auth.js
@@ -0,0 +1,29 @@
+const initialState = {
+  auth: [],
+};
+
+export function items(state = initialState, action) {
+  switch (action.type) {
+  case 'ADD_EVENTS':
+    return {
+      ...state,
+      items: [
+        ...state.items, {
+          text: action.fields.name.value,
+        },
+      ],
+    };
+
+  case 'DELETE_ITEM':
+    return {
+      ...state,
+      items: [
+        ...state.items.slice(0, action.index),
+        ...state.items.slice(+action.index + 1),
+      ],
+    };
+
+  default:
+    return state;
+  }
+}
---
+++ src/reducers/index.js
@@ -0,0 +1,12 @@
+import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
+import { items } from './items';
+
+const rootReducer = combineReducers({
+  form: formReducer,
+  /* your reducers */
+  items,
+  initAuth
+});
+
+export default rootReducer;
